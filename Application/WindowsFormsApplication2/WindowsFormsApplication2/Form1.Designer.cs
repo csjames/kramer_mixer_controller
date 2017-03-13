e65a16a8ee0b19db0c6e34446b25ea162d4cbeb0
Laurie Kirkcaldy - Shadow <lauriekirkcaldy@gmail.com>
2017-03-13 16:37:11 +0000

    Added in C# application (working for windows in taskbar/window format

---
+++ Application/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.input1 = new System.Windows.Forms.Button();
+            this.input2 = new System.Windows.Forms.Button();
+            this.input3 = new System.Windows.Forms.Button();
+            this.input4 = new System.Windows.Forms.Button();
+            this.input5 = new System.Windows.Forms.Button();
+            this.input6 = new System.Windows.Forms.Button();
+            this.input7 = new System.Windows.Forms.Button();
+            this.input8 = new System.Windows.Forms.Button();
+            this.output8 = new System.Windows.Forms.Button();
+            this.output7 = new System.Windows.Forms.Button();
+            this.output6 = new System.Windows.Forms.Button();
+            this.output5 = new System.Windows.Forms.Button();
+            this.output4 = new System.Windows.Forms.Button();
+            this.output3 = new System.Windows.Forms.Button();
+            this.output2 = new System.Windows.Forms.Button();
+            this.output1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mute = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pinned = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INPUTS";
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(12, 29);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(130, 51);
+            this.input1.TabIndex = 1;
+            this.input1.Text = "button1";
+            this.input1.UseVisualStyleBackColor = true;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(12, 86);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(130, 51);
+            this.input2.TabIndex = 2;
+            this.input2.Text = "button1";
+            this.input2.UseVisualStyleBackColor = true;
+            // 
+            // input3
+            // 
+            this.input3.Location = new System.Drawing.Point(12, 143);
+            this.input3.Name = "input3";
+            this.input3.Size = new System.Drawing.Size(130, 51);
+            this.input3.TabIndex = 3;
+            this.input3.Text = "button1";
+            this.input3.UseVisualStyleBackColor = true;
+            // 
+            // input4
+            // 
+            this.input4.Location = new System.Drawing.Point(12, 200);
+            this.input4.Name = "input4";
+            this.input4.Size = new System.Drawing.Size(130, 51);
+            this.input4.TabIndex = 4;
+            this.input4.Text = "button1";
+            this.input4.UseVisualStyleBackColor = true;
+            // 
+            // input5
+            // 
+            this.input5.Location = new System.Drawing.Point(12, 257);
+            this.input5.Name = "input5";
+            this.input5.Size = new System.Drawing.Size(130, 51);
+            this.input5.TabIndex = 5;
+            this.input5.Text = "button1";
+            this.input5.UseVisualStyleBackColor = true;
+            // 
+            // input6
+            // 
+            this.input6.Location = new System.Drawing.Point(12, 314);
+            this.input6.Name = "input6";
+            this.input6.Size = new System.Drawing.Size(130, 51);
+            this.input6.TabIndex = 6;
+            this.input6.Text = "button1";
+            this.input6.UseVisualStyleBackColor = true;
+            // 
+            // input7
+            // 
+            this.input7.Location = new System.Drawing.Point(12, 371);
+            this.input7.Name = "input7";
+            this.input7.Size = new System.Drawing.Size(130, 51);
+            this.input7.TabIndex = 7;
+            this.input7.Text = "button1";
+            this.input7.UseVisualStyleBackColor = true;
+            // 
+            // input8
+            // 
+            this.input8.Location = new System.Drawing.Point(12, 428);
+            this.input8.Name = "input8";
+            this.input8.Size = new System.Drawing.Size(130, 51);
+            this.input8.TabIndex = 8;
+            this.input8.Text = "button1";
+            this.input8.UseVisualStyleBackColor = true;
+            // 
+            // output8
+            // 
+            this.output8.Location = new System.Drawing.Point(148, 428);
+            this.output8.Name = "output8";
+            this.output8.Size = new System.Drawing.Size(130, 51);
+            this.output8.TabIndex = 17;
+            this.output8.Text = "button1";
+            this.output8.UseVisualStyleBackColor = true;
+            // 
+            // output7
+            // 
+            this.output7.Location = new System.Drawing.Point(148, 371);
+            this.output7.Name = "output7";
+            this.output7.Size = new System.Drawing.Size(130, 51);
+            this.output7.TabIndex = 16;
+            this.output7.Text = "button1";
+            this.output7.UseVisualStyleBackColor = true;
+            // 
+            // output6
+            // 
+            this.output6.Location = new System.Drawing.Point(148, 314);
+            this.output6.Name = "output6";
+            this.output6.Size = new System.Drawing.Size(130, 51);
+            this.output6.TabIndex = 15;
+            this.output6.Text = "button1";
+            this.output6.UseVisualStyleBackColor = true;
+            // 
+            // output5
+            // 
+            this.output5.Location = new System.Drawing.Point(148, 257);
+            this.output5.Name = "output5";
+            this.output5.Size = new System.Drawing.Size(130, 51);
+            this.output5.TabIndex = 14;
+            this.output5.Text = "button1";
+            this.output5.UseVisualStyleBackColor = true;
+            // 
+            // output4
+            // 
+            this.output4.Location = new System.Drawing.Point(148, 200);
+            this.output4.Name = "output4";
+            this.output4.Size = new System.Drawing.Size(130, 51);
+            this.output4.TabIndex = 13;
+            this.output4.Text = "button1";
+            this.output4.UseVisualStyleBackColor = true;
+            // 
+            // output3
+            // 
+            this.output3.Location = new System.Drawing.Point(148, 143);
+            this.output3.Name = "output3";
+            this.output3.Size = new System.Drawing.Size(130, 51);
+            this.output3.TabIndex = 12;
+            this.output3.Text = "button1";
+            this.output3.UseVisualStyleBackColor = true;
+            // 
+            // output2
+            // 
+            this.output2.Location = new System.Drawing.Point(148, 86);
+            this.output2.Name = "output2";
+            this.output2.Size = new System.Drawing.Size(130, 51);
+            this.output2.TabIndex = 11;
+            this.output2.Text = "button1";
+            this.output2.UseVisualStyleBackColor = true;
+            // 
+            // output1
+            // 
+            this.output1.Location = new System.Drawing.Point(148, 29);
+            this.output1.Name = "output1";
+            this.output1.Size = new System.Drawing.Size(130, 51);
+            this.output1.TabIndex = 10;
+            this.output1.Text = "button1";
+            this.output1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "OUTPUTS";
+            // 
+            // mute
+            // 
+            this.mute.Location = new System.Drawing.Point(12, 485);
+            this.mute.Name = "mute";
+            this.mute.Size = new System.Drawing.Size(266, 51);
+            this.mute.TabIndex = 18;
+            this.mute.Text = "Mute Channel";
+            this.mute.UseVisualStyleBackColor = true;
+            this.mute.Click += new System.EventHandler(this.mute_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Minimized to system tray";
+            this.notifyIcon1.BalloonTipTitle = "iKramer_x86";
+            this.notifyIcon1.Icon = global::WindowsFormsApplication2.Properties.Resources.notifyIcon;
+            this.notifyIcon1.Text = "iKramer_x86";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pinned
+            // 
+            this.pinned.AutoSize = true;
+            this.pinned.Location = new System.Drawing.Point(269, 2);
+            this.pinned.Name = "pinned";
+            this.pinned.Size = new System.Drawing.Size(98, 21);
+            this.pinned.TabIndex = 19;
+            this.pinned.Text = "checkBox1";
+            this.pinned.UseVisualStyleBackColor = true;
+            this.pinned.Visible = false;
+            this.pinned.MouseLeave += new System.EventHandler(this.pinned_onMouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 547);
+            this.Controls.Add(this.pinned);
+            this.Controls.Add(this.mute);
+            this.Controls.Add(this.output8);
+            this.Controls.Add(this.output7);
+            this.Controls.Add(this.output6);
+            this.Controls.Add(this.output5);
+            this.Controls.Add(this.output4);
+            this.Controls.Add(this.output3);
+            this.Controls.Add(this.output2);
+            this.Controls.Add(this.output1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.input8);
+            this.Controls.Add(this.input7);
+            this.Controls.Add(this.input6);
+            this.Controls.Add(this.input5);
+            this.Controls.Add(this.input4);
+            this.Controls.Add(this.input3);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.label1);
+            this.Icon = global::WindowsFormsApplication2.Properties.Resources.notifyIcon;
+            this.Name = "Form1";
+            this.Text = "iKramer_x86";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button input1;
+        private System.Windows.Forms.Button input2;
+        private System.Windows.Forms.Button input3;
+        private System.Windows.Forms.Button input4;
+        private System.Windows.Forms.Button input5;
+        private System.Windows.Forms.Button input6;
+        private System.Windows.Forms.Button input7;
+        private System.Windows.Forms.Button input8;
+        private System.Windows.Forms.Button output8;
+        private System.Windows.Forms.Button output7;
+        private System.Windows.Forms.Button output6;
+        private System.Windows.Forms.Button output5;
+        private System.Windows.Forms.Button output4;
+        private System.Windows.Forms.Button output3;
+        private System.Windows.Forms.Button output2;
+        private System.Windows.Forms.Button output1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button mute;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox pinned;
+    }
+}
+
---
+++ Application/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        Button[] inputButtons = new Button[8];
+        Button[] outputButtons = new Button[8];
+        HttpClient client = new HttpClient();
+
+        static private int NUM_IO = 8;
+
+        int input = -1;
+        int output = -1;
+
+        String[] colorArray =
+            {"#71E096",
+            "#F5A26F",
+            "#A98CFF",
+            "#FF89B5",
+            "#668DE5",
+            "#90D4F7",
+            "#FFDC89",
+            "#ED6D79",
+            "#FFFFFF",
+            "#FFFFFF",
+            "#FFFFFF",
+            "#FFFFFF",
+            "#FFFFFF",
+            "#FFFFFF",
+            "#FFFFFF",
+            "#BBBBBB"};
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            
+
+            client.BaseAddress = new Uri("http://10.0.10.1:9000/");
+
+            inputButtons[0] = input1;
+            inputButtons[1] = input2;
+            inputButtons[2] = input3;
+            inputButtons[3] = input4;
+            inputButtons[4] = input5;
+            inputButtons[5] = input6;
+            inputButtons[6] = input7;
+            inputButtons[7] = input8;
+
+            outputButtons[0] = output1;
+            outputButtons[1] = output2;
+            outputButtons[2] = output3;
+            outputButtons[3] = output4;
+            outputButtons[4] = output5;
+            outputButtons[5] = output6;
+            outputButtons[6] = output7;
+            outputButtons[7] = output8;
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            syncJSON(new CancellationToken());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            for (int i = 0; i < NUM_IO; i++)
+            {
+                int j = i;
+                inputButtons[i].Click += new EventHandler((s, ev) =>
+                {
+                    if (input == j)
+                    {
+                        input = -1;
+                        fade(-1, inputButtons);
+                    }
+                    else
+                    {
+                        input = j;
+                        fade(j, inputButtons);
+                    }
+                    checkMap();
+                });
+
+                outputButtons[i].Click += new EventHandler((s, ev) =>
+                {
+                    if (output == j)
+                    {
+                        output = -1;
+                        fade(-1, outputButtons);
+                    }
+                    else
+                    {
+                        output = j;
+                        fade(j, outputButtons);
+                    }
+                    checkMap();
+                });
+
+                int argb = Int32.Parse(colorArray[i].Replace("#", "FF"), System.Globalization.NumberStyles.HexNumber);
+                inputButtons[i].BackColor = Color.FromArgb(argb);
+                inputButtons[i].TabStop = false;
+                inputButtons[i].FlatStyle = FlatStyle.Flat;
+                inputButtons[i].FlatAppearance.BorderSize = 0;
+                inputButtons[i].FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+
+
+                outputButtons[i].TabStop = false;
+                outputButtons[i].FlatStyle = FlatStyle.Flat;
+                outputButtons[i].FlatAppearance.BorderSize = 0;
+                outputButtons[i].FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+            }
+
+            int argb2 = Int32.Parse(colorArray[15].Replace("#", "FF"), System.Globalization.NumberStyles.HexNumber);
+            mute.BackColor = Color.FromArgb(argb2);
+            mute.TabStop = false;
+            mute.FlatStyle = FlatStyle.Flat;
+            mute.FlatAppearance.BorderSize = 0;
+            mute.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+            Console.WriteLine("Done the things");
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                minimize();
+            }
+        }
+
+        private void minimize()
+        {
+            Console.WriteLine("Minimizing");
+            notifyIcon1.Visible = true;
+            //notifyIcon1.ShowBalloonTip(3000);
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+        }
+
+
+        private void mute_Click(object sender, EventArgs e)
+        {
+            if(input==15)
+            {
+                input = -1;
+                fade(-1, inputButtons);
+                return;
+            }
+            input = 15;
+            fade(17, inputButtons); // Make none of them selected
+            checkMap();
+        }
+
+        private void checkMap()
+        {
+            Console.WriteLine("Checking for map(+1): " + input + " " + output);
+            if (input > -1 && output > -1)
+            {
+                Console.WriteLine("Mapping(+1) " + input + " to " + output);
+                map(input+1, output+1);
+                input = -1;
+                output = -1;
+                fade(input, inputButtons);
+                fade(output, outputButtons);
+            }
+        }
+
+        private void map(int x, int y)
+        {
+            string mapstr = "map?in=" + x + "&out=" + y;
+            HttpResponseMessage response = client.GetAsync(mapstr).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Mapped " + x + " to " + y);
+                int argb = Int32.Parse(colorArray[x-1].Replace("#", "FF"), System.Globalization.NumberStyles.HexNumber);
+                outputButtons[y-1].BackColor = Color.FromArgb(argb);
+                if(!pinned.Checked) minimize();
+            } else
+            {
+                MessageBox.Show("Fail", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        async Task syncJSON(CancellationToken token)
+        {
+            while (true)
+            {
+                await Task.Delay(200, token);
+                if (input > -1 || output > -1) continue;
+                HttpResponseMessage response = client.GetAsync("state").Result;
+                string result = response.Content.ReadAsStringAsync().Result;
+
+                JObject jsonobj = JObject.Parse(result);
+                Dictionary<string, JObject> dict = JsonConvert.DeserializeObject<Dictionary<string, JObject>>(result);
+
+                for (int i = 1; i < NUM_IO + 1; i++)
+                {
+                    JObject port;
+                    if (dict.TryGetValue(i.ToString(), out port))
+                    {
+                        JToken obj;
+                        if (port.TryGetValue("Name", out obj))
+                        {
+                            inputButtons[i - 1].Text = (string)obj;
+                            outputButtons[i - 1].Text = (string)obj;
+                        }
+                        if (port.TryGetValue("MappedTo", out obj))
+                        {
+                            int mapped = int.Parse((string)obj);
+                           // Console.WriteLine("Coloured " + i + " to " + mapped);
+                            if (mapped > 16) continue;
+                            int argb = Int32.Parse(colorArray[mapped - 1].Replace("#", "FF"), System.Globalization.NumberStyles.HexNumber);
+                            outputButtons[i - 1].BackColor = Color.FromArgb(argb);
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+        private void fade(int selected,Button[] buttons)
+        {
+            for (int i = 0; i < NUM_IO; i++)
+            {
+                buttons[i].BackColor = Color.FromArgb((i==selected || selected == -1) ? 255 : 50, buttons[i].BackColor.R, buttons[i].BackColor.G, buttons[i].BackColor.B);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(pinned.Bounds.Contains(e.Location) && !pinned.Visible)
+            {
+                pinned.Visible = true;
+            }
+        }
+        private void pinned_onMouseLeave(object sender, EventArgs e)
+        {
+            pinned.Visible = false;
+        }
+
+    }
+}
